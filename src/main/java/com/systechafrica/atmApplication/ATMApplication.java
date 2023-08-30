f0738f8a12a8373a9caa394e36397228b3a9879c
IsabelNzioka <isabelnzioka@gmail.com>
2023-08-30 18:52:50 +0300

    Using private methods for the transactions - enhance readability

--- src/main/java/com/systechafrica/atmApplication/ATMApplication.java
+++ src/main/java/com/systechafrica/atmApplication/ATMApplication.java
@@ -5,26 +5,81 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ATMApplication {
-  
-
-    public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
 
         // defaults
         String defaultPassword = "Admin123";
         double balance = 1000.0;
 
         int attemptsCount = 3;
-
         boolean isUserActive = true;
         double withdrawCharges = 0.0;
 
         StringBuilder receipt = new StringBuilder();
-       
 
 
-        // 
-        while (attemptsCount > 0) {
+    private void withdrawalTransaction() {
+            System.out.print("Enter amount to withdraw: ");
+            int amount = scanner.nextInt();
+            double transactionFee = ( 2.0/100 * amount);
+
+             if( (balance - amount) < 0) {
+             System.out.print("Insufficient Amount \n");
+              } else {
+                        
+              // withdrawCharges = withdrawCharges +  ( 2.0/100 * amount); // for the bank
+               balance = balance - amount - transactionFee;
+                System.out.println("Your new balance is: " + balance);
+                 }
+
+                // TODO use flat to fomat the receipts transactions.
+                receipt.append("WITHDRAWAL  AMOUNT:      ").append(amount).append("\n");
+                 receipt.append("TRANSACTION FEE:        ").append(transactionFee).append("\n");
+    }
+
+
+    private void depositTransactions() {
+          System.out.print("Enter the deposit amount: ");
+           int amount = scanner.nextInt();
+
+            balance = balance + amount;
+            System.out.println("Your new balance is: " + balance);
+
+             // Appending the transactions to the Stringbuilder to send them as a receipt to the user
+            String amountFormatingUsingFlat = String.format("DEPOSIT AMOUNT: %20d%n", amount);
+            receipt.append(amountFormatingUsingFlat);
+
+    }
+    
+    private void transferFundsTRansactions() {
+            System.out.print("Enter the transfer amount: ");
+            int amount = scanner.nextInt();
+            balance = balance - amount;
+            System.out.println("Your new balance is: " + balance);
+
+              // Appending the transactions to the Stringbuilder to send them as a receipt to the user
+            String amountFormatingUsingFlat = String.format("TRANSFER AMOUNT: %20d%n", amount);
+            receipt.append(amountFormatingUsingFlat);
+
+          
+    }
+    
+   private void printReceits(){
+        System.out.println("--------------------  RECEIPT --------------- ");
+        System.out.println("*********************************************");
+
+        String amountFormatingUsingFlat = String.format("YOUR BALANCE IS: %20.2f%n", balance);
+        receipt.append(amountFormatingUsingFlat);
+
+        System.out.println(receipt);               
+        System.out.println("*********************************************");
+
+   }
+   
+   
+   
+    public void  atmTransactions() throws InterruptedException {
+          while (attemptsCount > 0) {
             System.out.print("Please enter your PASSWORD: ");
             String password = scanner.nextLine();
 
@@ -50,63 +105,26 @@ public class ATMApplication {
                        int option = scanner.nextInt();
                        scanner.nextLine();
 
-                        // TODO --------- USE SWITCH STATEMENTS FRO THE VARIOUS OPTIONS.
+                        // TODO --------- TRY USING SWITCH STATEMENTS FRO THE VARIOUS OPTIONS.
 
                     if (option == 1) {
                         System.out.println("Your balance is: Ksh " + balance);
                         Thread.sleep(3000);
                         
                     } else if (option == 2) {
-                        System.out.print("Enter the deposit amount: ");
-                        int amount = scanner.nextInt();
-
-                        balance = balance + amount;
-                        System.out.println("Your new balance is: " + balance);
-
-                        // Appending the transactions to the Stringbuilder to send them as a receipt to the user
-                        String amountFormatingUsingFlat = String.format("DEPOSIT AMOUNT: %20d%n", amount);
-                        receipt.append(amountFormatingUsingFlat);
+                        depositTransactions();
                         Thread.sleep(3000);
                     } else if (option == 3) {
-                        System.out.print("Enter amount to withdraw: ");
-                        int amount = scanner.nextInt();
-                        double transactionFee = ( 2.0/100 * amount);
-
-                        if( (balance - amount) < 0) {
-                          System.out.print("Insufficient Amount \n");
-                         } else {
-                        
-                        // withdrawCharges = withdrawCharges +  ( 2.0/100 * amount); // for the bank
-                        balance = balance - amount - transactionFee;
-                        System.out.println("Your new balance is: " + balance);
-                         }
-
-                        // TODO use flat to fomat the receipts transactions.
-                         receipt.append("WITHDRAWAL  AMOUNT: ").append(amount).append("\n");
-                         receipt.append("TRANSACTION FEE:  ").append(transactionFee).append("\n");
+                        withdrawalTransaction();
                          Thread.sleep(3000);
                        
                     } else if (option == 4) {
-                         System.out.print("Enter the transfer amount: ");
-                        int amount = scanner.nextInt();
-                        balance = balance - amount;
-                        System.out.println("Your new balance is: " + balance);
-
-                        receipt.append("TRANSFER AMOUNT: ").append(amount).append("\n");
+                        transferFundsTRansactions();
                         Thread.sleep(3000);
                         
                     } else if (option == 5) {
                         // Quit - isUserActive = false;
-                        System.out.println("--------------------  RECEIPT --------------- ");
-                        System.out.println("*********************************************");
-
-
-                        String amountFormatingUsingFlat = String.format("YOUR BALANCE IS: %20.2f%n", balance);
-                        receipt.append(amountFormatingUsingFlat);
-
-                        System.out.println(receipt);
-                        
-                        System.out.println("*********************************************");
+                        printReceits();
                         break; 
 
                     } else {
@@ -116,10 +134,7 @@ public class ATMApplication {
                         System.out.println("Invalid input. Please enter a valid option as a number.");
                         scanner.nextLine();
                     }
-                  
-
-
-                   
+                    
                 }
 
                 break;
@@ -131,5 +146,17 @@ public class ATMApplication {
         }
 
         scanner.close();
+
+    }
+  
+
+    public static void main(String[] args) throws InterruptedException {
+      ATMApplication app = new ATMApplication();
+      app.atmTransactions();
+       
+
+
+        // 
+      
     }
 }
