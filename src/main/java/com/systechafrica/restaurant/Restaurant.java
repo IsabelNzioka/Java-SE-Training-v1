22dcab75336948010502cb39fcf0ecb24668ae70
IsabelNzioka <isabelnzioka@gmail.com>
2023-09-10 00:44:34 +0300

    checking tests

--- src/main/java/com/systechafrica/restaurant/Restaurant.java
+++ src/main/java/com/systechafrica/restaurant/Restaurant.java
@@ -3,10 +3,8 @@ package com.systechafrica.restaurant;
 public class Restaurant {
 
     public static void main(String[] args) {
-        
-
-
         Order order = new Order();
+        
         order.setAndazi(100);
         order.setChai(200);
 
